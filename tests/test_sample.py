4fa2a2632a9bf6c870ceae43c23aecbc7adb8b81
Rick <rick.staa@outlook.com>
2019-07-04 16:05:59 +0200

    Edit CCI config file and run build test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,12 +3,12 @@ Content of test_sample.py
 """
 def func(x):
     """
-    Function
+    Function to be tested
     """
     return x + 1
 
 def test_answer():
     """
-    Answer
+    Test function
     """
     assert func(3) == 4
