a245d4d7906cd6b41cebdc9526d841dc1aae9a60
Rick <rick.staa@outlook.com>
2019-07-09 15:56:18 +0200

    Added gqcnn_perception_test.py

---
+++ scripts/gqccn_perception_test.py
@@ -0,0 +1,38 @@
+import perception
+import cv2
+import matplotlib.pyplot as plt
+
+kinect_camera = perception.Kinect2Sensor()
+kinect_camera.start()
+
+while True:
+    # Get frames
+    color_im, depth_im, _ = kinect_camera.frames()
+
+    # Display images
+    #fig, axes = plt.subplots(1, 2)
+    #for ax, im in zip(axes, [color_im.data, depth_im.data]):
+    #    ax.imshow(im)
+    #    ax.axis('off')
+    #plt.show()
+
+    ## Create segmask ##
+    bin_im = color_im.to_binary()
+
+    # Create segmask
+    fg_detector = RgbdForegroundMaskDetector()
+    bg_detector = 
+
+    # Create mask
+    #bin_img = color_im.to_binary()
+    #imgplot = plt.imshow(bin_img)
+    cv2.imshow("color", color_im.data)
+    cv2.imshow("depth", depth_im.data)
+    cv2.imshow("ir", ir_im.data)
+    cv2.imshow("bin_img", bin_im.data)
+    key = cv2.waitKey(delay=1)
+    if key == ord('q'):
+        break
+
+# Close camera connection
+kinect_camera.stop()
