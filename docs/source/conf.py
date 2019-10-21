f50057ed479dbd6bc879d34ea68a2dd8ccb38399
Rick <rick.staa@outlook.com>
2019-10-21 12:21:38 +0200

    Bump version: 0.0.4 → 0.0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = "Rick Staa"
 
 # The full version, including alpha/beta/rc tags
 version = "0.0"
-release = "0.0.4"
+release = "0.0.5"
 
 # -- General configuration ---------------------------------------------------
 
